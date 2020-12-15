28d7d4f03c224d26c02417400d8e4594ed30a0d5
RedstoneParadox <spinjitzu00@gmail.com>
2020-12-15 09:52:10 -0800

    Setup

---
+++ src/main/java/io/github/redstoneparadox/serverbulletin/ServerBulletin.kt
@@ -0,0 +1,10 @@
+package io.github.redstoneparadox.serverbulletin
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("unused")
+object ServerBulletin: ModInitializer {
+    override fun onInitialize() {
+        println("Hello world!")
+    }
+}
